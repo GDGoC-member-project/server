d7bbcc59f8c5f9d18bb9a01d6dffcb988a10e02c
curo0618@gmail.com <curo0618@gmail.com>
2026-01-03 20:53:11 +0900

    feat: add Spring Boot member application

---
+++ backend/member/src/main/java/com/gdgoc/member/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.gdgoc.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 이 클래스가 RESTful API 컨트롤러임을 선언한다.
+public class HelloController {
+
+    @GetMapping("/hello") // HTTP GET 요청이 "/hello" 경로로 들어올 때 이 메서드를 실행한다.
+    public String hello() {
+        return "Hello, World!";
+    }
+}
