d2ba107afc63917de0a19141fa7520ce4d845c5d
Jisung <jisung050901@hanmail.net>
2026-01-05 00:42:27 +0900

    securityConfig update auth+project in one file

--- backend/member/src/main/java/com/gdgoc/member/security/SecurityConfig.java
+++ backend/member/src/main/java/com/gdgoc/member/security/SecurityConfig.java
@@ -30,7 +30,6 @@ public class SecurityConfig {
                                     "Invalid or missing token.",
                                     null
                             );
-
                             try {
                                 new ObjectMapper().writeValue(response.getWriter(), body);
                             } catch (Exception ex) {
