d4f39de15b5950b7f25ced1f2896764abbaf0e64
Dohyeop Lim <dhlim0817@gmail.com>
2026-01-09 21:45:22 +0900

    fix: make OAuth2 success handler to create user and profile on first login

--- src/main/java/com/gdgoc/member/security/OAuth2SuccessHandler.java
+++ src/main/java/com/gdgoc/member/security/OAuth2SuccessHandler.java
@@ -1,7 +1,10 @@
 package com.gdgoc.member.security;
 
+import com.gdgoc.member.account.Role;
 import com.gdgoc.member.account.UserAuth;
 import com.gdgoc.member.account.UserAuthRepository;
+import com.gdgoc.member.domain.profile.entity.Profile;
+import com.gdgoc.member.domain.profile.repository.ProfileRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,17 +20,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final UserAuthRepository userAuthRepository;
+    private final ProfileRepository profileRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
-        UserAuth userAuth = userAuthRepository.findByExternalUid(oidcUser.getName()).orElseThrow();
+        UserAuth userAuth = userAuthRepository.findByExternalUid(oidcUser.getSubject()).orElseGet(() -> {
+            UserAuth newUser = new UserAuth(
+                    UUID.randomUUID(),
+                    oidcUser.getSubject(),
+                    oidcUser.getEmail(),
+                    null,
+                    Role.MEMBER
+            );
+            userAuthRepository.save(newUser);
+
+            Profile newProfile = new Profile(
+                    newUser.getUserId(),
+                    oidcUser.getEmail(),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+            profileRepository.save(newProfile);
+            return newUser;
+        });
 
         ClassPathResource resource = new ClassPathResource("static/oauth_success.html");
         Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
